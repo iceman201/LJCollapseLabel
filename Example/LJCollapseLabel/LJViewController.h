b3a96250c210c5b7c331c0729f175f500d360b2f
Liguo Jiao <liguo@jiao.co.nz>
2018-11-26 14:00:00 +0100

    Initial commit

---
+++ Example/LJCollapseLabel/LJViewController.h
@@ -0,0 +1,13 @@
+//
+//  LJViewController.h
+//  LJCollapseLabel
+//
+//  Created by iceman201 on 12/07/2018.
+//  Copyright (c) 2018 iceman201. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LJViewController : UIViewController
+
+@end
---
+++ Example/LJCollapseLabel/LJViewController.m
@@ -0,0 +1,29 @@
+//
+//  LJViewController.m
+//  LJCollapseLabel
+//
+//  Created by iceman201 on 12/07/2018.
+//  Copyright (c) 2018 iceman201. All rights reserved.
+//
+
+#import "LJViewController.h"
+
+@interface LJViewController ()
+
+@end
+
+@implementation LJViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

fdac4292c173762fa836ef839d329147b15374d3
Liguo Jiao <liguo@jiao.co.nz>
2018-11-29 04:00:00 +0100

    Update Header file

---
+++ LJCollapseLabel/Classes/public/CollapseLabel.h
@@ -0,0 +1,10 @@
+//
+//  CollapseLabel.h
+//  CollapseLabel
+//
+//  Created by Liguo Jiao on 11/11/18.
+//  Copyright © 2018 Liguo Jiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LJCollapseLabel.h"

3ce067d36ea7222ca5c5766721d413ace6769a06
Liguo Jiao <liguo@jiao.co.nz>
2018-12-07 02:25:35 +1300

    Initial commit

---
+++ Example/LJCollapseLabel/LJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LJAppDelegate.h
+//  LJCollapseLabel
+//
+//  Created by iceman201 on 12/07/2018.
+//  Copyright (c) 2018 iceman201. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LJCollapseLabel/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LJCollapseLabel
+//
+//  Created by iceman201 on 12/07/2018.
+//  Copyright (c) 2018 iceman201. All rights reserved.
+//
+
+@import UIKit;
+#import "LJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LJAppDelegate class]));
+    }
+}

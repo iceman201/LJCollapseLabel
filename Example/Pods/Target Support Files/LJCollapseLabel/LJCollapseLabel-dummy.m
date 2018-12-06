93df11dbb5a1a0211c0ea7422b884bd6d2bd49af
Liguo Jiao <liguo@jiao.co.nz>
2018-12-07 02:32:50 +1300

    init

---
+++ Example/Pods/Target Support Files/LJCollapseLabel/LJCollapseLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LJCollapseLabel : NSObject
+@end
+@implementation PodsDummy_LJCollapseLabel
+@end
---
+++ Example/Pods/Target Support Files/Pods-LJCollapseLabel_Example/Pods-LJCollapseLabel_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LJCollapseLabel_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LJCollapseLabel_Example
+@end
